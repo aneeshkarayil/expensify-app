73222bff9d16c9e10ca7a4b089041ed7962929fb
aneeshkarayil <aneeshkarayil@gmail.com>
2020-05-04 10:28:28 +0530

    styling list

--- public/src/components/ExpenseList.js
+++ public/src/components/ExpenseList.js
@@ -3,18 +3,21 @@ import { connect } from "react-redux";
 import ExpenseLineItem from "./ExpenseListItem";
 import selectExpense from "../selectors/expenses";
 
-export const ExpenseList = props => (
-  <div>
-    <h1>Expense List</h1>
-    {props.expenses.map(expense => {
+export const ExpenseList = (props) => (
+  <div className="content-container">
+    <div className="list-header">
+      <div className="show-for-desktop">Expense</div>
+      <div className="show-for-desktop">Amount</div>
+    </div>
+    {props.expenses.map((expense) => {
       return <ExpenseLineItem key={expense.id} {...expense} />;
     })}
   </div>
 );
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    expenses: selectExpense(state.expenses, state.filters)
+    expenses: selectExpense(state.expenses, state.filters),
   };
 };
 
